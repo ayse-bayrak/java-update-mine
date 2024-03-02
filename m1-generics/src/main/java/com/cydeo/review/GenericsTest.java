47074e17f4a3be4f992829a40a995ec7d3bf01a9
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-03 00:54:53 +0300

    Generic review is studied and create review package and GenericTest, Student, Teacher and Person Classes

--- m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
+++ m1-generics/src/main/java/com/cydeo/review/GenericsTest.java
@@ -13,10 +13,14 @@ public class GenericsTest {
         studentList.add(new Student("Joe", 102));
         studentList.add(new Student("Adam", 103));
 
+        //=======================================================
+
         List<Teacher> teacherList = new ArrayList<>();
 
         teacherList.add(new Teacher("Albus", 201));
         teacherList.add(new Teacher("Severus", 202));
+
+        //=======================================================
 
         List<Person> personList = new ArrayList<>();
 
@@ -46,15 +50,16 @@ public class GenericsTest {
 
     }
 
-    // List<Object> -> list have -> 1, "string", Student            -> eliminated (List<T>)
-    // List<Object> -> list have -> Student1, Student2, Student3
-    // List<Object> -> list have -> 1, 2, 3                         -> eliminated (List<T extends Student>)
-    // List<Object> -> list have -> "str1", "str2", "str3"          -> eliminated (List<T extends Student>)
-    // List<T>      -> list have -> "str1", "str2", "str3"
-    // List<T>      -> list have -> 1, 2, 3
-    // List<T>      -> list have -> Student1, Student2, Student3
-    // List<T extends Student>      -> list have -> Student1, Student2, Student3
-    // List<T extends Person>       -> list have -> Person, Student, Teacher
+    //  List<Object> -> list have -> 1, "string", Student            -> eliminated (List<T>)
+    //  List<Object> -> list have -> Student1, Student2, Student3
+    //  List<Object> -> list have -> 1, 2, 3                         -> eliminated (List<T extends Student>)
+    //  List<Object> -> list have -> "str1", "str2", "str3"          -> eliminated (List<T extends Student>)
+    //  List<T>      -> list have -> "str1", "str2", "str3"
+    //  List<T>      -> list have -> 1, 2, 3
+    //  List<T>      -> list have -> Student1, Student2, Student3
+    //  List<T extends Student>      -> list have -> Student1, Student2, Student3
+    //  List<T extends Person>       -> list have -> Person, Student, Teacher
+
     public static <T extends Person> void printInfo2(List<T> personList) {
 
         for (T eachPerson: personList) {
