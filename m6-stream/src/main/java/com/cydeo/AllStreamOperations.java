0579b70002fa2aa3ff1eb3f51495b3ebf1b55d48
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-04 14:33:09 +0300

    StreamIntermediateOperations is done

--- m6-stream/src/main/java/com/cydeo/AllStreamOperations.java
+++ m6-stream/src/main/java/com/cydeo/AllStreamOperations.java
@@ -39,11 +39,14 @@ public class AllStreamOperations {
 
         //
 
-        // STREAM INTERMEDIATE OPERATION
-
+        //for the test of intermediate operation, we need to close the stream,
+        // so firstly let's learn one of the Terminal Operation ==> forEach()
 
         List<Integer> list = Arrays.asList(1,2,3,4);
-        list.forEach(System.out::println);
+        // list.forEach(System.out::println);// this is coming from the iterable interface
+
+        //Intermediate operations
+        // All the intermediate operation return Stream
 
         //FILTER ()
         System.out.println("Filter");
@@ -51,18 +54,18 @@ public class AllStreamOperations {
                 .filter(i-> i%2==0)  // Intermediate Operation 1, and you need to close the stream
                 .forEach(System.out::println); // I am closing stream
 
-        //DISTINCT do not show me the duplicate
+        //DISTINCT- Filtereing unique element- do not show me the duplicate
         System.out.println("Distinct");
 
         Stream<Integer> str = list.stream()
                 .filter(i -> i%2 == 0) // intermediate operation
                 .distinct(); // intermediate operation, stream is open still
 
-        str.forEach(System.out::println);  // terminal operation
-        //All the intermadiate operation return the Stream
+        str.forEach(System.out::println);  //stream close, terminal operation, it is not MUST intermediate operation, you can directly terminal operations
+        //All the intermediate operation return the Stream
         //Terminal operation forEach() return void
 
-        //LIMIT(n)
+        //LIMIT(n)-Trancating Stream
         System.out.println("limit()");
 
         list.stream()
@@ -78,13 +81,18 @@ public class AllStreamOperations {
                 .skip(1)// still stream is open
                 .forEach(System.out::println);
 
-
-        //map is used to do something `each element`
+        //MAPPING
+        //if you want to certain things on each, certain actions with map
+        //map is used to do something `each element`and we are changing something, return something rather than Stream
         System.out.println("map()");
         list.stream()
                 .map(number -> number*2)
                 .filter(i-> i%3 ==0)
                 .forEach(System.out::println); // terminal operator
+
+//THIS MAPPING AND FILTER in the development environment we are going to use a lot,
+        //because we always did for loop and if statement
+
 
         // STREAM TERMINAL OPERATION
 
--- m6-stream/src/main/java/com/cydeo/streamIntroOperations/StreamIntermediateOperations.java
+++ m6-stream/src/main/java/com/cydeo/streamIntroOperations/StreamIntermediateOperations.java
@@ -6,11 +6,14 @@ import java.util.stream.Stream;
 
 public class StreamIntermediateOperations {
     public static void main(String[] args) {
-
-        //Intermediate operations
+        //for the test of intermediate operation, we need to close the stream,
+        // so firstly let's learn one of the Terminal Operation ==> forEach()
 
         List<Integer> list = Arrays.asList(1,2,3,4);
-        list.forEach(System.out::println);
+       // list.forEach(System.out::println);// this is coming from the iterable interface
+
+        //Intermediate operations
+        // All the intermediate operation return Stream
 
         //FILTER ()
         System.out.println("Filter");
@@ -18,18 +21,18 @@ public class StreamIntermediateOperations {
                 .filter(i-> i%2==0)  // Intermediate Operation 1, and you need to close the stream
                 .forEach(System.out::println); // I am closing stream
 
-        //DISTINCT do not show me the duplicate
+        //DISTINCT- Filtereing unique element- do not show me the duplicate
         System.out.println("Distinct");
 
         Stream<Integer> str = list.stream()
                 .filter(i -> i%2 == 0) // intermediate operation
                 .distinct(); // intermediate operation, stream is open still
 
-        str.forEach(System.out::println);  // terminal operation
-        //All the intermadiate operation return the Stream
+        str.forEach(System.out::println);  //stream close, terminal operation, it is not MUST intermediate operation, you can directly terminal operations
+        //All the intermediate operation return the Stream
         //Terminal operation forEach() return void
 
-        //LIMIT(n)
+        //LIMIT(n)-Trancating Stream
         System.out.println("limit()");
 
         list.stream()
@@ -45,14 +48,17 @@ public class StreamIntermediateOperations {
                 .skip(1)// still stream is open
                 .forEach(System.out::println);
 
-
-        //map is used to do something `each element`
+        //MAPPING
+        //if you want to certain things on each, certain actions with map
+        //map is used to do something `each element`and we are changing something, return something rather than Stream
         System.out.println("map()");
         list.stream()
                 .map(number -> number*2)
                 .filter(i-> i%3 ==0)
                 .forEach(System.out::println); // terminal operator
 
+//THIS MAPPING AND FILTER in the development environment we are going to use a lot,
+        //because we always did for loop and if statement
 
     }
 }
