93c2705e83b12830a67940c812e4a1af64ebb919
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-05 16:50:41 +0300

    Java9 Factor-CompactString-Modularity-PrivateMethodInterface

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/CompactStringDemo.java
@@ -0,0 +1,15 @@
+package com.cydeo.java9;
+
+public class CompactStringDemo {
+    String abc = "cydeo";
+
+    /*
+    public final class String implements...... {
+    private final byte[] value;
+    private final byte coder;
+
+
+
+    }
+     */
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/ModuleDemo.java
@@ -0,0 +1,16 @@
+package com.cydeo.java9;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ModuleDemo {
+    public static void main(String[] args) {
+
+
+        System.out.println(String.class.getSimpleName());
+        System.out.println(List.class.getSimpleName());
+        System.out.println(Scanner.class.getSimpleName());
+
+
+    }
+}
