ee3db9622b3f07b2b3369238bfbb9c65c6f0cf19
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-05 11:31:37 +0300

    Optional Class is covered and added some changes PairsNumbersTask

---
+++ m6-stream/src/main/java/com/cydeo/task/square/PairsOfNumbersYE.java
@@ -0,0 +1,18 @@
+package com.cydeo.task.square;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PairsOfNumbersYE {
+    public static void main(String[] args) {
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<int[]> pairs = new ArrayList<>();
+        numbers1.forEach(x -> numbers2.stream() // we can forEach() without .stream()
+                        .map(y -> new int[]{x, y})
+                        .forEach(pairs::add) // we can use method reference
+                );
+        pairs.forEach(p -> System.out.println(Arrays.toString(p)));
+    }
+}
---
+++ m7-optional-bigdecimal/src/main/java/com/cydeo/OptionalDemo.java
@@ -0,0 +1,33 @@
+package com.cydeo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalDemo {
+    //it comes with Java 8 and basically Optional is a new Type
+    public static void main(String[] args) {
+
+
+        List<Integer> number = Arrays.asList(2, 4, 2, 10, 23);
+        //empty()- isPresent()
+
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);//Optional.empty
+        System.out.println(empty.isPresent());//False
+
+        System.out.println(Optional.of(number).isPresent());//True
+
+        //ifPresent
+        Optional<Integer> bigNumber = number.stream()
+                .filter(x-> x>100).findAny();
+        bigNumber.ifPresent(System.out::println); //nothing
+
+        // get
+    //    System.out.println(bigNumber.get()); // No value present
+
+        //orElse
+        System.out.println(bigNumber.orElse(5));//5
+
+    }
+}
