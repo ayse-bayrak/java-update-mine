22e05cf30960f8e5f548d3e05e8dd9e84141be74
aysegunesbayrak <aysegunes.bayrak@gmail.com>
2024-03-06 18:43:01 +0300

    Java14- record

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,17 @@
+package com.cydeo.java14;
+
+public record User (String name, String address)  {
+    // all field private final
+    //this is immutable, this is final, that's why there is no Setter,
+    // That is one of the handicap
+    // you need Setter, specially UI perspective
+
+    //You have:
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+
+
+}
---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/UserTest.java
@@ -0,0 +1,8 @@
+package com.cydeo.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User("Mike", "VA");  // we have constructor
+        System.out.println(user.name()); // diractly call orijinal name do not need to get
+    }
+}
